789631601bc5f649506873cff94de9f684aca87d
Adam Nowatkowski <adam.nowatkowski@gmail.com>
2023-09-11 22:32:25 +0200

    paths vip

--- src/app/ui/atoms/ProductCounter.tsx
+++ src/app/ui/atoms/ProductCounter.tsx
@@ -1,8 +1,32 @@
 "use client";
 
-import { type ReactNode, useState } from "react";
+import {
+	useParams,
+	usePathname,
+	useRouter,
+	useSearchParams,
+} from "next/navigation";
+import { type ReactNode, useState, useEffect } from "react";
 
 export const ProductCounter = ({ children }: { children: ReactNode }) => {
+	useParams();
+	useSearchParams();
+	usePathname();
+
+	console.log(useRouter()); //ciekawostka pokazuje wszystko co jest w routerze w konsoli
+	const router = useRouter();
+
+	const isLoggedIn = () => {
+		return false;
+	};
+	useEffect(() => {
+		if (isLoggedIn()) {
+			router.replace("/dashboard");
+		}
+	}, [router]);
+	// sometimes it is better to not use useEffect and use this instead:
+	// if (isLoggedIn()) {router.replace("/dashboard")}
+
 	const [counter, setCounter] = useState(0);
 	return (
 		<div>
