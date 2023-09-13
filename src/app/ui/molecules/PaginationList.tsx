93a118b88bd98848581d35d551e86be0f3618e54
Adam Nowatkowski <adam.nowatkowski@gmail.com>
2023-09-13 16:36:46 +0200

    lil fixes

--- src/app/ui/molecules/PaginationList.tsx
+++ src/app/ui/molecules/PaginationList.tsx
@@ -1,30 +1,25 @@
 "use client";
 
-import clsx from "clsx";
+// import clsx from "clsx";
 import { ActiveLink } from "../atoms/ActiveLink";
 
 export const PaginationList = ({
 	paginationLenght,
-	currentPagination,
+	// currentPagination,
 }: {
 	paginationLenght: number;
 	currentPagination: number;
 }) => {
-	const className =
-		" border-2 border-slate-900 rounded-md p-2 hover:bg-slate-500";
-	const activeClassName = "font-bold border-5 bg-slate-500";
 
 	return (
 		<div aria-label="Pagination" className="mt-8 flex justify-center space-x-2">
 			{Array.from(Array(paginationLenght - 1).keys()).map((pageNumber) => (
 				<ActiveLink
-					key={pageNumber}
-					href={"/products/" + (pageNumber + 1)}
-					className={clsx(
-						className,
-						currentPagination == pageNumber + 1 && activeClassName,
-					)}
-				>
+                    key={pageNumber}
+                    href={"/products/" + (pageNumber + 1)}
+                    className=" border-2 border-slate-900 rounded-md p-2 hover:bg-slate-500"
+                    activeClassName = "font-bold border-5 bg-slate-500"
+                >
 					{pageNumber + 1}
 				</ActiveLink>
 			))}
