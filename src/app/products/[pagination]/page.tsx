75cd7263a5e55b8b58b85f832fcd532a4d2f7c1b
AdamNowatkowski <83035462+AdamNowatkowski@users.noreply.github.com>
2023-09-13 16:45:55 +0200

    Learning next (#5)
    * pretty new stuff
    
    * paths vip
    
    * Upgrades 2.0
    
    * test 2
    
    * pagination for products beta added
    
    * test_passed
    
    * last for today
    
    * lil fixes

--- src/app/products/[pagination]/page.tsx
+++ src/app/products/[pagination]/page.tsx
@@ -1,4 +1,4 @@
-import { notFound, usePathname } from "next/navigation";
+import { notFound } from "next/navigation";
 import { ProductList } from "../../ui/organisms/ProductList";
 import { getProductsList } from "../../../api/products";
 import { PaginationList } from "../../ui/molecules/PaginationList";
