b1201870bad8cab32c2c2415e6342daad513300a
AdamNowatkowski <adam.nowatkowski@gmail.com>
2023-09-17 10:12:52 +0200

    cosmetics

--- src/app/ui/atoms/ProductCoverImage.tsx
+++ src/app/ui/atoms/ProductCoverImage.tsx
@@ -6,13 +6,13 @@ export const ProductCoverImage = ({
 	alt: string;
 }) => {
 	return (
-		<div className="aspect-square overflow-hidden rounded-md border bg-slate-50 hover:bg-slate-100 ">
+		<div className="aspect-square overflow-hidden rounded-md border ">
 			<img
 				width={320}
 				height={320}
 				alt={alt}
 				src={src}
-				className="h-full w-full object-cover object-center p-4 transition-transform hover:scale-105"
+				className="h-full w-full object-contain object-center p-4 transition-transform hover:scale-105"
 			/>
 		</div>
 	);
