0c89e4cad79af181e9170fc97b66af2403f99888
Adam Nowatkowski <adam.nowatkowski@gmail.com>
2023-09-20 21:52:44 +0200

    sleep deleted

--- src/app/ui/organisms/SuggestedProducts.tsx
+++ src/app/ui/organisms/SuggestedProducts.tsx
@@ -1,10 +1,10 @@
 import { getProductsList } from "../../../api/products";
 import { ProductList } from "./ProductList";
 
-const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+// const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export const SuggestedProductsList = async () => {
     const products = await getProductsList();
-    await sleep(1000);
+    // await sleep(1000);
     return <ProductList products={products.slice(-4)} />
 };
