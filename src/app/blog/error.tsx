84ba4a093c7a0e905dceea6df7787a86a07deb55
Adam Nowatkowski <adam.nowatkowski@gmail.com>
2023-09-28 18:50:36 +0200

    testy 55

--- src/app/blog/error.tsx
+++ src/app/blog/error.tsx
@@ -1,5 +1,5 @@
 "use client"
-export default function ErrorPage({error , reset}: {error: Error & {digest: string}, reset: () => void}) {
+export default function ErrorPage({error }: {error: Error & {digest: string}}) {
 	return (
 		<div className="flex h-screen w-screen items-center justify-center">
 			<div className="h-12 w-12 animate-bounce bg-red-500">ERROR {error.digest}</div>
