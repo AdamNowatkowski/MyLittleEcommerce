66e86daa6e5e53f1af1fa678d9e38b47b02b2fb1
Adam Nowatkowski <adam.nowatkowski@gmail.com>
2023-10-13 16:18:36 +0200

    test id fix

--- src/app/ui/organisms/SuggestedProducts.tsx
+++ src/app/ui/organisms/SuggestedProducts.tsx
@@ -1,15 +1,23 @@
 import { notFound } from "next/navigation";
-import { ProductList } from "./ProductList";
+import { ProductListItem } from "@/app/ui/molecules/ProductListItem";
 import { getProductsByCategorySlug } from "@/api/products";
 
-// const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export const SuggestedProductsList = async ({category} :  { category: string }) => {
-    // const products = await getProductsList();
     const products = await getProductsByCategorySlug(category.toLowerCase())
 	if (!products) {
 		throw notFound();
 	}
-    // await sleep(1000);
-    return <ProductList data-testid="related-products" products={products.slice(-4)} />
+
+    const productslist = products.slice(-4)
+    return (
+		<ul
+			className="grid grid-cols-1 gap-8 sm:grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
+			data-testid="related-products"
+		>
+			{productslist.map((product) => {
+				return <ProductListItem key={product.id} product={product} />;
+			})}
+		</ul>
+	);
 };
