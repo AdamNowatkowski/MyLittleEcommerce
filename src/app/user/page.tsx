a55a067d0d606c33719f1c53ba88404c65f5e549
Adam Nowatkowski <adam.nowatkowski@gmail.com>
2026-07-11 13:30:10 +0200

    Fix syntax error from disabled Clerk comments

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -4,22 +4,4 @@ import { redirect } from "next/navigation";
 
 export default async function UserPage() {
 	redirect("/sign-in");
-	/*
-	const user = await currentUser();
-	if (!user) {
-		redirect("/sign-in");
-	}
-	return (
-		<div>
-			<div className="mb-3 mt-2 text-2xl">
-				<Link href={"/orders"}>
-					<h1>{user.firstName}&rsquo;s Orders Link</h1>
-				</Link>
-			</div>
-			<div className="flex items-center justify-center">
-				<UserProfile />
-			</div>
-		</div>
-	);
-	*/
 }
